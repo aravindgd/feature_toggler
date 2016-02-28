f9b0b5af676fea636cf475127f7865add6c0319f
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2016-02-29 04:19:45 +0530

    added deps, information about the page for publishing as hex package

--- mix.exs
+++ mix.exs
@@ -1,34 +1,50 @@
 defmodule FeatureToggler.Mixfile do
-  use Mix.Project
+	use Mix.Project
 
-  def project do
-    [app: :feature_toggler,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
-  end
+	def project do
+		[app: :feature_toggler,
+			version: "0.0.1",
+			elixir: "~> 1.2",
+			description: description,
+			package: package,
+			build_embedded: Mix.env == :prod,
+			start_permanent: Mix.env == :prod,
+			deps: deps]
+	end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger]]
-  end
+	# Configuration for the OTP application
+	#
+	# Type "mix help compile.app" for more information
+	def application do
+		[applications: [:logger]]
+	end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [{:exredis, ">= 0.2.2"},
-      {:earmark, "~> 0.1", only: :dev},
-       {:ex_doc, "~> 0.11", only: :dev}]
-  end
+	defp description do
+		"""
+    This is a simple feature toggler/switch with redis database written in elixir.
+		"""
+	end
+
+	defp package do
+		[# These are the default files included in the package
+			files: ["lib", "config", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+			maintainers: ["Aravind Gopalakrishnan"],
+			licenses: ["MIT License"],
+			links: %{"GitHub" => "https://github.com/aravindgd/feature_toggler"}]
+	end
+
+	# Dependencies can be Hex packages:
+	#
+	#   {:mydep, "~> 0.3.0"}
+	#
+	# Or git/path repositories:
+	#
+	#   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+	#
+	# Type "mix help deps" for more examples and options
+	defp deps do
+		[{:exredis, ">= 0.2.2"},
+			{:earmark, "~> 0.1", only: :dev},
+			{:ex_doc, "~> 0.11", only: :dev}]
+	end
 end
